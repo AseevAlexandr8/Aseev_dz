5acfabf69054ac5017c15bbff42413b027f44934
Alexandr <aseevalexandr@bk.ru>
2022-04-11 14:53:43 +0300

    дз 1

---
+++ Aseev_dz_1.py
@@ -0,0 +1,34 @@
+# # task 1
+duration = int(input('введите число в секундах : '))
+if duration <= 60:
+    print( duration , 'сек')
+elif duration <= 3600:
+    s = duration , 'сек'
+    m = duration // 60 , 'мин'
+    print(s , m )
+elif duration <= 86400 :
+    s = duration , 'сек'
+    m = duration // 60 , 'мин'
+    h = duration // (60*60*24) , 'час'
+    print( s , m , h)
+elif duration >= 86401 :
+    s = duration , 'сек'
+    m = duration // 60 , 'мин'
+    h = duration // (60*60*24) , 'час'
+    d = duration // 86400 , 'дн'
+    print(s , m , h , d )
+
+# #task 2
+list_number = [num for num in range(1 , 1000 , 2)]
+two_list = [list_number * list_number * list_number for list_number in list_number]
+print(two_list)
+
+# task 3
+percent = int(input('Введите число процентов :'))
+if percent == 1:
+    word = 'процент'
+elif percent <= 4:
+    word = 'процента'
+else:
+    word = 'процентов'
+    print(percent, word)
